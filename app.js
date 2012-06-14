b675aa45a966dd31578fc7a9bedabb750975616a
Michael Ihle <michael.ihle@gmail.com>
2012-06-13 20:01:21 -0700

    added structure to site

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Requires
 var application_root = __dirname,
     express = require("express"),
     path = require("path"),
@@ -6,11 +7,9 @@ var application_root = __dirname,
 var app = express.createServer();
 
 // Database
-
-mongoose.connect('mongodb://localhost/ecomm_database');
+mongoose.connect('mongodb://heroku_app5235183:osnln2uihco341kvcv37dn7rak@ds033897.mongolab.com:33897/heroku_app5235183');
 
 // Config
-
 app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -19,23 +18,85 @@ app.configure(function () {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-
-// Routes
-
-app.get('/api', function (req, res) {
-  res.send('Cardsharp API is running');
-});
-
-// Test code
-
-app.get('/', function(request, response) {
-  response.send('Card Sharp landing page!');
-});
-
-
 // Launch server
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+// Schema
+var Schema = mongoose.Schema;  
+
+var Product = new Schema({  
+    title: { type: String, required: true },  
+    description: { type: String, required: true },  
+    style: { type: String, unique: true },  
+    modified: { type: Date, default: Date.now }
+});
+
+
+var ProductModel = mongoose.model('Product', Product);  
+
+// API Routes
+app.get('/api', function (req, res) {
+  res.send('Cardsharp API is running');
+});
+
+
+app.get('/api/products', function (req, res){
+  return ProductModel.find(function (err, products) {
+    if (!err) {
+      return res.send(products);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+app.post('/api/products', function (req, res){
+  var product;
+  console.log("POST: ");
+  console.log(req.body);
+  product = new ProductModel({
+    title: req.body.title,
+    description: req.body.description,
+    style: req.body.style,
+  });
+  product.save(function (err) {
+    if (!err) {
+      return console.log("created");
+    } else {
+      return console.log(err);
+    }
+  });
+  return res.send(product);
+});
+
+app.get('/api/products/:id', function (req, res){
+  return ProductModel.findById(req.params.id, function (err, product) {
+    if (!err) {
+      return res.send(product);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+app.delete('/api/products/:id', function (req, res){
+  return ProductModel.findById(req.params.id, function (err, product) {
+    return product.remove(function (err) {
+      if (!err) {
+        console.log("removed");
+        return res.send('');
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+
+
+
+
