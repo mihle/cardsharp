1c78639d8c26a002f081a5ecf0237944fc37e1f1
Michael Ihle <michael.ihle@gmail.com>
2012-06-13 19:18:36 -0700

    added new node modules

---
+++ app.js
@@ -0,0 +1,28 @@
+var application_root = __dirname,
+    express = require("express"),
+    path = require("path"),
+    mongoose = require('mongoose');
+
+var app = express.createServer();
+
+// Database
+
+mongoose.connect('mongodb://localhost/ecomm_database');
+
+// Config
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.get('/api', function (req, res) {
+  res.send('Ecomm API is running');
+});
+
+// Launch server
+
+app.listen(4242);
